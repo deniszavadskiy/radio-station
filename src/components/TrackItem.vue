7275b1d8f2fc5c20a57b6de0ee0ff2cf4ee3f6b1
Denis Zavadskiy <deniszavadskiy@Deniss-MacBook-Pro.local>
2023-09-28 00:12:16 +0300

    add(TrackItem.vue)

---
+++ src/components/TrackItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="history-track">
+    <img :src="track.imageUrl" alt="track artwork" class="history-track__artwork">
+    <div class="history-track__meta">
+      <h3>{{ track.title }}</h3>
+      <p>{{ track.artist }}</p>
+      <p class="history-track__meta__duration">{{ track.duration }}</p>
+    </div>
+    <p class="history-track__time">{{ trackStartTime }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Track } from '@/services/radio.service';
+import { computed } from 'vue';
+
+const props = defineProps<{ track: Track }>();
+
+const trackStartTime = computed(() => new Date(props.track.time).toLocaleTimeString())
+
+</script>
+
+<style lang="scss" scoped>
+.history-track {
+  height: 6rem;
+  display: flex;
+
+  &__artwork {
+    margin-right: 1.5rem;
+    border-radius: 5px;
+  }
+
+  &__meta {
+    display: flex;
+    flex-direction: column;
+    margin-right: 1.5rem;
+
+    &__duration {
+      font-size: 0.75rem;
+      margin-top: auto;
+    }
+  }
+
+  &__time {
+    margin-left: auto;
+    font-size: 0.75rem;
+  }
+}
+</style>
