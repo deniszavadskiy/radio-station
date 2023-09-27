412273397d51b7ab6a3b47501cb3d4f7c9885c34
Denis Zavadskiy <deniszavadskiy@Deniss-MacBook-Pro.local>
2023-09-27 19:48:26 +0300

    update(components): added necessary components

---
+++ src/components/NowPlayingTrack.vue
@@ -0,0 +1,15 @@
+<template>
+    <div playing-track v-if="track">
+        <img :src="track.imageUrl" alt="track artwork">
+        <h3>{{ track.title }}</h3>
+        <p>{{ track.artist }}</p>
+        <p>{{ track.duration }}</p>
+    </div>
+</template>
+<script setup lang="ts">
+import type { Track } from '@/services/radio.service';
+
+const props = defineProps<{ track: Track | null }>()
+
+</script>
+<style lang="scss" scoped></style>
---
+++ src/components/RadioApp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="radio-app">
+        <NowPlayingTrack :track="nowPlayingTrack"></NowPlayingTrack>
+        <TracksHistory :tracks="historyTracks"></TracksHistory>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { type Track } from '@/services/radio.service';
+import NowPlayingTrack from './NowPlayingTrack.vue';
+import TracksHistory from './TracksHistory.vue';
+import { computed } from 'vue';
+
+const props = defineProps<{ tracks: Track[] }>();
+
+const nowPlayingTrack = computed<Track | null>(() => props.tracks.find(({ status }) => status === 'playing') || null);
+const historyTracks = computed<Track[]>(() => props.tracks.filter(({ status }) => status === 'history'));
+</script>
+
+<style lang="scss" scoped></style>
