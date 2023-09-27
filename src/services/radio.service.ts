6b3d6817b079f2c553b44417cdba780234b16c83
Denis Zavadskiy <deniszavadskiy@Deniss-MacBook-Pro.local>
2023-09-27 19:50:53 +0300

    add(radio.service.ts): add radio service to fetch tracks

---
+++ src/services/radio.service.ts
@@ -0,0 +1,33 @@
+const serverURL = 'http://localhost:3000';
+
+export interface Track {
+  artist: string;
+  title: string;
+  duration: string;
+  iTunesTrackUrl: string;
+  imageUrl: string;
+  status: 'history' | 'playing';
+  time: string;
+}
+
+export async function fetchTracks(): Promise<{ nowplaying: Track[] }> {
+  try {
+    const response = await fetch(`${serverURL}/fetch-radio-data`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+
+  return { nowplaying: [] };
+}
